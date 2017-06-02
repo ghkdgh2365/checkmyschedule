2011b647ef9a728ae1a3b3c43fb9ad739a3423ba
ghkdgh2365 <ghkdgh2365@gmail.com>
2017-06-02 03:56:35 +0000

    day 타입 바꿈

--- db/migrate/20170529032259_create_posts.rb
+++ db/migrate/20170529032259_create_posts.rb
@@ -2,7 +2,7 @@ class CreatePosts < ActiveRecord::Migration
   def change
     create_table :posts do |t|
       t.text :content
-      t.date :day
+      t.text :day
       t.string :checking
 
       t.timestamps null: false
--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema.define(version: 20170529032259) do
 
   create_table "posts", force: :cascade do |t|
     t.text     "content"
-    t.date     "day"
+    t.text     "day"
     t.string   "checking"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
